eed00dfb14bcbf0a6b08cdf5bae2dbf671373e56
Dhruv <dhruvsakariya2304@gmail.com>
2022-07-26 21:33:11 +0530

    basic template and pages added

---
+++ src/components/Navbar/App.js
@@ -0,0 +1,223 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+// import { homePageState } from "../HomePage/reduxSlice";
+// import { userState } from "../User/reduxSlice";
+// import { logoutUserAsync } from "../auth/Logout/reduxSlice.js";
+
+// other laibrary
+import { useNavigate } from "react-router-dom";
+
+// Bootstrap
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Dropdown from "react-bootstrap/Dropdown";
+import Container from "react-bootstrap/Container";
+
+// Icon
+import {
+  BsFillPersonFill,
+  BsSearch,
+  BsFillBellFill,
+  BsFillPeopleFill,
+  BsImages,
+} from "react-icons/bs";
+import { IoIosLogOut, IoMdCalendar } from "react-icons/io";
+import { IoSettingsSharp } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { Icon } from "@iconify/react";
+
+// Css
+// import styles from "./App.module.css";
+import "./App.css";
+
+// assets
+import logo from "../../assets/svg/logo.svg";
+import avatar from "../../assets/img/avatar.jpg";
+
+export default function NavbarComponent() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  //   const { userAvl } = useSelector(userState);
+
+  const userAvl = true;
+
+  const dropDownHandler = (event, navigateStr) => {
+    event.preventDefault();
+    navigate(navigateStr);
+  };
+
+  const ActionMenu = () => {
+    if (userAvl) {
+      const logoutHandler = (event) => {
+        event.preventDefault();
+        // dispatch(logoutUserAsync());
+        navigate("/login");
+      };
+      return (
+        <Nav className="flex-row me-0">
+          <Dropdown className="w-auto h-auto m-auto px-2 ">
+            <Dropdown.Toggle
+              className="border  "
+              variant=""
+              id="userNavigation"
+            >
+              <img
+                src={avatar}
+                width="35"
+                height="35"
+                className="d-inline-block align-top m-1"
+                alt="React Bootstrap logo"
+              />
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="  shadow position-absolute border mt-2">
+              <Dropdown.Item
+                href="/"
+                onClick={(e) => {
+                  dropDownHandler(e, "/");
+                }}
+                className="  "
+              >
+                <Icon icon="fluent:desktop-32-regular" />
+                Home
+              </Dropdown.Item>
+              <Dropdown.Divider className="p-0 mt-1" />
+
+              <Dropdown.Item
+                href="/"
+                onClick={(e) => {
+                  dropDownHandler(e, "/user/profile");
+                }}
+                className="  "
+              >
+                <CgProfile /> Profile
+              </Dropdown.Item>
+              <Dropdown.Item
+                href="/"
+                onClick={(e) => {
+                  dropDownHandler(e, "/Gallary");
+                }}
+                className="  "
+              >
+                <BsImages /> Gallary
+              </Dropdown.Item>
+              <Dropdown.Divider className="p-0 m-0" />
+              <Dropdown.Item href="/" onClick={logoutHandler} className="  ">
+                <IoIosLogOut /> Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      );
+    } else {
+      return (
+        <Nav className="flex-row me-0">
+          <Link to="/login" className="px-3 ">
+            Login
+          </Link>
+          <Link to="/register" className="px-3 ">
+            Register
+          </Link>
+        </Nav>
+      );
+    }
+  };
+
+  return (
+    // <Navbar
+    //   className="sticky-top py-1 px-2 d-flex justify-content-between   border-bottom"
+    //   expand="lg"
+    // >
+    //   <Navbar id="basic-navbar-nav">
+    //     <Nav className="ml-auto">
+    // <Navbar.Brand className="ms-0 ">
+    //   <img
+    //     src={logo}
+    //     width="auto"
+    //     height="30"
+    //     className="d-inline-block align-top"
+    //     alt="React Bootstrap logo"
+    //   />
+    // </Navbar.Brand>
+    //       <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+    //     </Nav>
+    //   </Navbar>
+    //   <Nav className="me-auto ms-3">
+    //         <Nav.Link href="#home">Home</Nav.Link>
+    //         <Nav.Link href="#features">About us</Nav.Link>
+    //         <Nav.Link href="#pricing">Contact us</Nav.Link>
+    //       </Nav>
+    //   <ActionMenu />
+    // </Navbar>
+    <>
+      <Navbar key={"sm"} bg="light" expand={"sm"} className="mb-3">
+        <Navbar.Brand
+          className="ms-0  brandLogo"
+          onClick={(e) => {
+            dropDownHandler(e, "/");
+          }}
+        >
+          <img
+            src={logo}
+            width="auto"
+            height="30"
+            className="d-inline-block align-top m-1"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-sm`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-start flex-grow-1 pe-3">
+              <Nav.Link
+                href="/"
+                onClick={(e) => {
+                  dropDownHandler(e, "/Projects");
+                }}
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="/"
+                onClick={(e) => {
+                  dropDownHandler(e, "/About-us");
+                }}
+              >
+                About us
+              </Nav.Link>
+              <Nav.Link
+                href="/"
+                onClick={(e) => {
+                  dropDownHandler(e, "/Contact-us");
+                }}
+              >
+                Contact us
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+        <div className="d-flex  flex-row-reverse ">
+          <Navbar.Toggle
+            className="me-2"
+            aria-controls={`offcanvasNavbar-expand-sm`}
+          />
+          <ActionMenu />
+        </div>
+      </Navbar>
+    </>
+  );
+}
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+
+// router
+import { Routes, Route } from "react-router-dom";
+
+// components
+import NavbarComponent from "../components/Navbar/App";
+import Projects from "../components/Projects/App";
+import ContactUs from "../components/ContactUs/App";
+import AboutUs from "../components/AboutUs/App";
+import User from "../components/User/App";
+import Gallary from "../components/Gallary/App";
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <>
+      <NavbarComponent />
+      <Routes>
+        <Route>
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/About-us" element={<ContactUs />} />
+          <Route path="/Contact-us" element={<AboutUs />} />
+          <Route path="/user/profile" element={<User />} />
+          <Route path="/Gallary" element={<Gallary />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
-export default Root
+export default Root;
