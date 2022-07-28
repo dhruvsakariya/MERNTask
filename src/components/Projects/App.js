16a0bb6c502fcd456a0219bc63487a553acef813
Dhruv <dhruvsakariya2304@gmail.com>
2022-07-28 16:23:31 +0530

    added all project image and links live

--- src/components/Projects/App.js
+++ src/components/Projects/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import one from "../../assets/img/one.webp";
+import two from "../../assets/img/2.jpg";
+import three from "../../assets/img/3.jpg";
+import four from "../../assets/img/4.jpg";
+import five from "../../assets/img/5.jpg";
+import six from "../../assets/img/6.jpg";
+import seven from "../../assets/img/7.jpg";
+import eight from "../../assets/img/8.jpg";
+
 const Projects = () => {
   return (
     <div className="homePageWrapper py-4">
@@ -10,168 +19,128 @@ const Projects = () => {
             <div className="row gy-4 justify-content-center">
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={one} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://bizshare.netlify.app/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 1"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={two} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://dhruvsakariya.github.io/Portfolio/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 2"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={three} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://nodejsshopapp.herokuapp.com/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 3"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={four} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="http://gratitude-wall.netlify.app/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 4"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={five} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://blog-post-pink.vercel.app/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 5"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={six} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://blog-post-pink.vercel.app/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 6"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={seven} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://bizchat.netlify.app/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 7"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
                     </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
-                    </a>
                   </div>
                 </div>
               </div>
               <div className="col-xl-3 col-lg-4 col-md-6">
                 <div className="gallery-item h-100">
-                  <img
-                    src="https://source.unsplash.com/400x300"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={eight} className="img-fluid" alt="" />
                   <div className="gallery-links d-flex align-items-center justify-content-center">
                     <a
-                      href="https://source.unsplash.com/400x300"
+                      href="https://threeas.netlify.app/"
+                      target={"_blank"}
+                      rel="noreferrer"
                       title="Gallery 8"
                       className="glightbox preview-link"
                     >
                       <i className="bi bi-arrows-angle-expand"></i>
-                    </a>
-                    <a href="gallery-single.html" className="details-link">
-                      <i className="bi bi-link-45deg"></i>
                     </a>
                   </div>
                 </div>
